ee40470f1c7240c76aa87a3e15e7bfcdcc251c65
Anton Bondar <s0ul.tr4d3r@gmail.com>
2021-01-31 16:57:44 +0300

    10 lesson
    промежуточный коммит. 10 уроков.

---
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { React, Component } from "react";
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Technologies />
+    </div>
+  );
+};
+
+
+
+
+export default App;
